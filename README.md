648cef381d463f3e9a48398463ae31fbb23f9b07
alvaro-8a <alvaroblanco8a@gmail.com>
2022-06-13 15:13:42 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,167 @@
+# Fund Me Hardhat Project
+
+<div id="top"></div>
+
+
+[![MIT License][license-shield]][license-url]
+[![LinkedIn][linkedin-shield]][linkedin-url]
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+Here's a blank template to get started: To avoid retyping too much info. Do a search and replace with your text editor for the following: `alvaro-8a`, `hardhat-fund-me-fcc`, `alvaro_8a_`, `alvaro-blanco-ochoa-9b14561a9`, `gmail`, `alvaroblanco8a`, `Crowdfunding Smart Contract`, `project_description`
+
+All the code was develop for learning from the [Learn Blockchain, Solidity, and Full Stack Web3 Development with JavaScript – 32-Hour Course](https://www.youtube.com/watch?v=gyMwXuJrbJQ&lis) from freeCodeCamp.org and Patrick Collins
+
+The project represents a smart contract for crowdfunding, where people can send money to the contract (getting their wallets and amount sent stored inside the smart contract) and only the owner of the contract can withdraw the funds
+
+It includes tests for the contracts and deployment functions for hardhat and real networks (ethereum, rinkeby, polygon...)
+
+This project demonstrates an advanced Hardhat use case, integrating other tools commonly used alongside Hardhat in the ecosystem.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Etherscan verification
+
+This project also includes automatic verification of the contract for real networks. The verification is only executed when running on real networks
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+### Built With
+
+* [Node.js](https://nodejs.org/)
+* [Solidity](https://docs.soliditylang.org/)
+* [Hardhat](https://hardhat.org/)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+Make sure you have already installed Node.js in your device. In case you have, upgrade npm to the latest version
+
+* npm
+  ```sh
+  npm install npm@latest -g
+  ```
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/alvaro-8a/hardhat-fund-me-fcc.git
+   ```
+2. Install NPM packages
+   ```sh
+   npm install
+   ```
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+You can use this code it as a template to develop your own funding contract, there are many improvements that can be done so feel free to make any change.
+
+This repo can also be used to learn about testing Smart Contract on hardhat, there are some tests written to understand the basics but you can write new one or redesign mines to fit you. 
+
+You can also use it to deploy your own crowdfunding smart contract to a Solidity supported Blockchain (Ethereum, Polygon, BSC...).
+
+**Note: This project is a demo and I don't recommend using it without changes, there are many things that can be improved so take in consideration before using it.**
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- ROADMAP -->
+## Roadmap
+
+- [✔️] Deployment scripts for local and real networks
+- [✔️] Verify contract on real networks
+- [✔️] Tested on local (Hardhat)
+- [✔️] Tested on a real network (Rinkeby)
+
+See the [open issues](https://github.com/alvaro-8a/hardhat-fund-me-fcc/issues) for a full list of proposed features (and known issues).
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion or an improvement that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Your Name - [@alvaro_8a_](https://twitter.com/alvaro_8a_) - alvaroblanco8a@gmail.com
+
+Project Link: [https://github.com/alvaro-8a/hardhat-fund-me-fcc](https://github.com/alvaro-8a/hardhat-fund-me-fcc)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+* [Learn Blockchain, Solidity, and Full Stack Web3 Development with JavaScript – 32-Hour Course](https://www.youtube.com/watch?v=gyMwXuJrbJQ&lis)
+* [Patrick Collins](https://www.youtube.com/c/PatrickCollins)
+* [FreeCodeCamp.org](https://www.youtube.com/c/Freecodecamp)
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[contributors-shield]: https://img.shields.io/github/contributors/alvaro-8a/hardhat-fund-me-fcc.svg?style=for-the-badge
+[contributors-url]: https://github.com/alvaro-8a/hardhat-fund-me-fcc/graphs/contributors
+[forks-shield]: https://img.shields.io/github/forks/alvaro-8a/hardhat-fund-me-fcc.svg?style=for-the-badge
+[forks-url]: https://github.com/alvaro-8a/hardhat-fund-me-fcc/network/members
+[stars-shield]: https://img.shields.io/github/stars/alvaro-8a/hardhat-fund-me-fcc.svg?style=for-the-badge
+[stars-url]: https://github.com/alvaro-8a/hardhat-fund-me-fcc/stargazers
+[issues-shield]: https://img.shields.io/github/issues/alvaro-8a/hardhat-fund-me-fcc.svg?style=for-the-badge
+[issues-url]: https://github.com/alvaro-8a/hardhat-fund-me-fcc/issues
+[license-shield]: https://img.shields.io/github/license/alvaro-8a/hardhat-fund-me-fcc.svg?style=for-the-badge
+[license-url]: https://github.com/alvaro-8a/hardhat-fund-me-fcc/blob/master/LICENSE.txt
+[linkedin-shield]: https://img.shields.io/badge/-LinkedIn-black.svg?style=for-the-badge&logo=linkedin&colorB=555
+[linkedin-url]: https://linkedin.com/in/alvaro-blanco-ochoa-9b14561a9
+[product-screenshot]: images/screenshot.png
---
+++ test/unit/FundMe.test.js
@@ -0,0 +1,219 @@
+const { assert, expect } = require("chai")
+const { deployments, ethers, getNamedAccounts } = require("hardhat")
+
+const { developmentChains } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe", async function () {
+          let fundMe
+          let deployer
+          let mockV3Aggregator
+          const sendValue = ethers.utils.parseEther("1") // 1 ETH
+          beforeEach(async () => {
+              // deploy our FundMe Contract
+              // using Hardhat-deploy
+              // const accounts = await ethers.getSigners()
+              // const accountZero = accounts[0]
+              deployer = (await getNamedAccounts()).deployer
+              await deployments.fixture(["all"])
+              fundMe = await ethers.getContract("FundMe", deployer)
+              mockV3Aggregator = await ethers.getContract("MockV3Aggregator")
+          })
+
+          describe("constructor", async function () {
+              it("Sets the aggregator addresses correctly", async function () {
+                  const response = await fundMe.getPriceFeed()
+                  assert.equal(response, mockV3Aggregator.address)
+              })
+          })
+
+          describe("fund", async function () {
+              it("Fails if you don't send enough ETH", async function () {
+                  await expect(fundMe.fund()).to.be.revertedWith(
+                      "Didn't send enough!"
+                  )
+              })
+              it("Updates the amount funded data structure", async function () {
+                  await fundMe.fund({ value: sendValue })
+                  const response = await fundMe.getAddressToAmountFunded(
+                      deployer
+                  )
+                  assert.equal(response.toString(), sendValue.toString())
+              })
+              it("Adds funder to array of getFunder", async function () {
+                  await fundMe.fund({ value: sendValue })
+                  const funder = await fundMe.getFunder(0)
+                  assert.equal(funder, deployer)
+              })
+          })
+
+          describe("withdraw", async function () {
+              beforeEach(async function () {
+                  await fundMe.fund({ value: sendValue })
+              })
+
+              it("Withdraw ETH from a single founder", async function () {
+                  // Arrange
+                  const startingFundMeBalance =
+                      await fundMe.provider.getBalance(fundMe.address)
+                  const startingDeployerBalance =
+                      await fundMe.provider.getBalance(deployer)
+                  // Act
+                  const transactionResponse = await fundMe.withdraw()
+                  const transactionReceive = await transactionResponse.wait(1)
+                  // Calculate the gas cost of the withdraw transaction to compare the endingDeployerBalance with the initial balance
+                  const { gasUsed, effectiveGasPrice } = transactionReceive
+                  const gasCost = gasUsed.mul(effectiveGasPrice)
+
+                  const endingFundMeBalance = await fundMe.provider.getBalance(
+                      fundMe.address
+                  )
+                  const endingDeployerBalance =
+                      await fundMe.provider.getBalance(deployer)
+                  // Assert
+                  assert.equal(endingFundMeBalance, 0)
+                  assert.equal(
+                      startingFundMeBalance
+                          .add(startingDeployerBalance)
+                          .toString(),
+                      endingDeployerBalance.add(gasCost).toString()
+                  )
+              })
+
+              it("Allows us to withdraw with multiple getFunder", async function () {
+                  // Arrange
+                  const accounts = await ethers.getSigners()
+                  for (let i = 1; i < 6; i++) {
+                      const fundMeConnectedContract = await fundMe.connect(
+                          accounts[i]
+                      )
+                      await fundMeConnectedContract.fund({ value: sendValue })
+                  }
+                  const startingFundMeBalance =
+                      await fundMe.provider.getBalance(fundMe.address)
+                  const startingDeployerBalance =
+                      await fundMe.provider.getBalance(deployer)
+
+                  // Act
+                  const transactionResponse = await fundMe.withdraw()
+                  const transactionReceive = await transactionResponse.wait(1)
+                  const { gasUsed, effectiveGasPrice } = transactionReceive
+                  const gasCost = gasUsed.mul(effectiveGasPrice)
+
+                  const endingFundMeBalance = await fundMe.provider.getBalance(
+                      fundMe.address
+                  )
+                  const endingDeployerBalance =
+                      await fundMe.provider.getBalance(deployer)
+
+                  // Assert
+                  assert.equal(endingFundMeBalance, 0)
+                  assert.equal(
+                      startingFundMeBalance
+                          .add(startingDeployerBalance)
+                          .toString(),
+                      endingDeployerBalance.add(gasCost).toString()
+                  )
+
+                  // Make sure the getFunder are reset properly
+                  await expect(fundMe.getFunder(0)).to.be.reverted
+
+                  for (i = 1; i < 6; i++) {
+                      assert.equal(
+                          await fundMe.getAddressToAmountFunded(
+                              accounts[i].address
+                          ),
+                          0
+                      )
+                  }
+              })
+
+              it("Only allows the owner to withdraw", async function () {
+                  const accounts = await ethers.getSigners()
+                  const attacker = accounts[1]
+                  const attackerConnectedContract = await fundMe.connect(
+                      attacker
+                  )
+                  await expect(
+                      attackerConnectedContract.withdraw()
+                  ).to.be.revertedWith("FundMe__NotOwner")
+              })
+
+              it("CheaperWithdraw ETH from a single founder", async function () {
+                  // Arrange
+                  const startingFundMeBalance =
+                      await fundMe.provider.getBalance(fundMe.address)
+                  const startingDeployerBalance =
+                      await fundMe.provider.getBalance(deployer)
+                  // Act
+                  const transactionResponse = await fundMe.cheaperWithdraw()
+                  const transactionReceive = await transactionResponse.wait(1)
+                  // Calculate the gas cost of the withdraw transaction to compare the endingDeployerBalance with the initial balance
+                  const { gasUsed, effectiveGasPrice } = transactionReceive
+                  const gasCost = gasUsed.mul(effectiveGasPrice)
+
+                  const endingFundMeBalance = await fundMe.provider.getBalance(
+                      fundMe.address
+                  )
+                  const endingDeployerBalance =
+                      await fundMe.provider.getBalance(deployer)
+                  // Assert
+                  assert.equal(endingFundMeBalance, 0)
+                  assert.equal(
+                      startingFundMeBalance
+                          .add(startingDeployerBalance)
+                          .toString(),
+                      endingDeployerBalance.add(gasCost).toString()
+                  )
+              })
+
+              it("Cheaper withdraw test", async function () {
+                  // Arrange
+                  const accounts = await ethers.getSigners()
+                  for (let i = 1; i < 6; i++) {
+                      const fundMeConnectedContract = await fundMe.connect(
+                          accounts[i]
+                      )
+                      await fundMeConnectedContract.fund({ value: sendValue })
+                  }
+                  const startingFundMeBalance =
+                      await fundMe.provider.getBalance(fundMe.address)
+                  const startingDeployerBalance =
+                      await fundMe.provider.getBalance(deployer)
+
+                  // Act
+                  const transactionResponse = await fundMe.cheaperWithdraw()
+                  const transactionReceive = await transactionResponse.wait(1)
+                  const { gasUsed, effectiveGasPrice } = transactionReceive
+                  const gasCost = gasUsed.mul(effectiveGasPrice)
+
+                  const endingFundMeBalance = await fundMe.provider.getBalance(
+                      fundMe.address
+                  )
+                  const endingDeployerBalance =
+                      await fundMe.provider.getBalance(deployer)
+
+                  // Assert
+                  assert.equal(endingFundMeBalance, 0)
+                  assert.equal(
+                      startingFundMeBalance
+                          .add(startingDeployerBalance)
+                          .toString(),
+                      endingDeployerBalance.add(gasCost).toString()
+                  )
+
+                  // Make sure the getFunder are reset properly
+                  await expect(fundMe.getFunder(0)).to.be.reverted
+
+                  for (i = 1; i < 6; i++) {
+                      assert.equal(
+                          await fundMe.getAddressToAmountFunded(
+                              accounts[i].address
+                          ),
+                          0
+                      )
+                  }
+              })
+          })
+      })
